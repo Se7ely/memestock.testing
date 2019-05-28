6e2095d54674cbfa1426b5292c9bb6d5ee9de97e
Seifeldin Elsehely <seifeldin.elsehely97@eng-st.cu.edu.eg>
2019-05-28 10:06:51 +0200

    All non-functional files removed

--- MainTrials/generator.py
+++
@@ -1,3 +0,0 @@
-import math
-for i in range(1920):
-    print(" assign allroundkeysout["+str(1919-i)+"]=allroundkeys["+str(math.floor(i/32))+"]["+str(i%32)+"];")
--- MainTrials/randomtester.py
+++
@@ -1,3 +0,0 @@
-import random
-
-print(str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9)))
